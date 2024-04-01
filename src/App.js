c55c73468f03cf328717ad5f209b2eb6f61d1ffc
layekdip <dipankar.layek@gmail.com>
2024-04-02 00:17:54 +0530

    adding state function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+
 import {CORE_CONCEPTS} from './data.js';
-import Header from './components/Header.js'
+import Header from './components/Header/Header.js'
 import CoreConcepts from './components/CoreConcepts.js'
+import TabButton from './components/TabButton.js'
 
 function App() {
+
+let tabContent = 'Please click a button...';
+
+const [ selectedTopic, setSelectedTopic ] = useState('Please click a button...');
+
+function handleSelect(selectedButton){
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+}
+
   return (
     <div>
       <Header />
@@ -19,6 +32,16 @@ function App() {
                 <CoreConcepts {...CORE_CONCEPTS[2]} />
                 <CoreConcepts {...CORE_CONCEPTS[3]} />
             </ul>
+        </section>
+        <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+                <TabButton onSelect={() => handleSelect('component')}>Component</TabButton>
+                <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            </menu>
+            { selectedTopic }
         </section>
         <h2>Time to get started!</h2>
       </main>
